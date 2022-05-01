16595498d87b2834f4ac38f86a3882fa1d008cae
“PFilipeVen” <venancio.pfp@gmail.com>
2022-05-01 16:17:55 +0100

    first commit

---
+++ src/main/java/com/courierKata/CourierKataApplication.java
@@ -0,0 +1,13 @@
+package com.courierKata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierKataApplication.class, args);
+	}
+
+}
